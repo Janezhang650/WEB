a90180ac21cc960b12f73b84e6afbf073d91cc8c
Janezhang650 <48990446+Janezhang650@users.noreply.github.com>
2021-07-19 09:32:05 +0800

    Add files via upload

---
+++ JavaScript DOM编程艺术/chapter02/example.js
@@ -0,0 +1,22 @@
+var mood = "happy";
+var age = 12;
+
+var height = "about 5'10\" tall";
+
+alert(height);
+
+var year = 2021;
+var message = "This year is ";
+message += year;
+alert(message);
+
+if(1>2){
+    alert("The world has gone mad!");
+} else {
+    alert("All is well with the world");
+}
+
+
+
+alert(mood);
+alert(age);
